912e0d71c866b7e8db1a6180d14e130ac2043a49
chathura-de-silva <ac.chathura@gmail.com>
2023-10-03 15:59:40 +0530

    s

--- CopyMyReadme.py
+++ CopyMyReadme.py
@@ -45,7 +45,7 @@ if not line_exists:
         gitignore.write("\n"+line_to_append)
 
 
-github_username = "chathura_test"
+github_username = get_github_username()
 
 
 # Open the Markdown file for reading and writing ('r+')
